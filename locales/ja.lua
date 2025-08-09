71ecbf729a5ec504278be76b0592eafa590bebb5
TecnohShinonome <TecnohShinonome@users.noreply.github.com>
2025-08-09 18:25:47 +0900

    Lang:Japanese

--- locales/ja.lua
+++ locales/ja.lua
@@ -1,21 +1,21 @@
 local Translations = {
     notify = {
-        ["hud_settings_loaded"] = "HUD設定を読み込みました",
-        ["hud_restart"] = "HUDを再起動しています",
-        ["hud_start"] = "HUDを起動しました",
-        ["hud_command_info"] = "このコマンドは現在のHUD設定をリセットします",
-        ["load_square_map"] = "スクエアマップ読み込み中...",
-        ["loaded_square_map"] = "スクエアマップ読み込み完了",
-        ["load_circle_map"] = "サークルマップ読み込み中...",
-        ["loaded_circle_map"] = "サークルマップ読み込み完了",
-        ["cinematic_on"] = "シネマティックモードを有効にしました",
-        ["cinematic_off"] = "シネマティックモードを無効にしました",
-        ["engine_on"] = "エンジン始動",
-        ["engine_off"] = "エンジン停止",
-        ["low_fuel"] = "燃料が残りわずかです！",
+        ["hud_settings_loaded"] = "HUD設定を読み込みました！",
+        ["hud_restart"] = "HUDを再起動しています！",
+        ["hud_start"] = "HUDが起動しました！",
+        ["hud_command_info"] = "このコマンドは現在のHUD設定をリセットします！",
+        ["load_square_map"] = "スクエアマップを読み込み中…",
+        ["loaded_square_map"] = "スクエアマップが読み込まれました！",
+        ["load_circle_map"] = "サークルマップを読み込み中…",
+        ["loaded_circle_map"] = "サークルマップが読み込まれました！",
+        ["cinematic_on"] = "シネマティックモード オン！",
+        ["cinematic_off"] = "シネマティックモード オフ！",
+        ["engine_on"] = "エンジンが始動しました！",
+        ["engine_off"] = "エンジンが停止しました！",
+        ["low_fuel"] = "燃料が少なくなっています！",
         ["access_denied"] = "権限がありません！",
-        ["stress_gain"] = "ストレスを感じた！",
-        ["stress_removed"] = "リラックスした気分"
+        ["stress_gain"] = "ストレスが増加しています！",
+        ["stress_removed"] = "ストレスが軽減しました！"
     }
 }
 
